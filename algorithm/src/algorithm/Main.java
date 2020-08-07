4eea1cabd8eb119cbd250252dc73d423583c153b
hairh <hairh@DESKTOP-53J7LK2>
2020-08-07 23:23:43 +0900

    오늘 알고리즘문제

---
+++ algorithm/src/algorithm/Main.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+
+		
+		
+	}
+}
---
+++ algorithm/src/algorithm/Question9498.java
@@ -0,0 +1,37 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Question9498 {
+	public static void main(String[] args) {
+		
+	
+	int A;
+	Scanner scanner = new Scanner(System.in);
+	A = scanner.nextInt();
+	if(A>=90) {
+		
+		System.out.println("A");
+	}
+	else if(A>=80) {
+		
+		System.out.println("B");
+	}
+
+	else if(A>=70) {
+	
+		System.out.println("C");
+}
+
+	else if(A>=60) {
+	
+		System.out.println("D");
+}
+
+	else {
+	
+		System.out.println("F");
+		}
+	}
+}
+
