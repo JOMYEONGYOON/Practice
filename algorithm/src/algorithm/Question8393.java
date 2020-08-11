d8e2236e8ef4ebde90c294c1940340ec38253a00
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-08-11 21:05:57 +0900

    for반복문 공부
    n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.

---
+++ algorithm/src/algorithm/Question8393.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+
+	Scanner scanner = new Scanner(System.in);
+	int N;
+	int sum = 0;
+	N = scanner.nextInt();
+	for(int i=1;i<=N;i++) {
+		sum = i+sum;
+		
+		}
+	System.out.println(sum);
+	}
+}
