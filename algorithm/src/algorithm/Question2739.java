b55135071cc6315c690847f8ee0a4b07252a2dc5
hairh <hairh@DESKTOP-53J7LK2>
2020-08-11 18:42:50 +0900

    for를 이용한 구구단

---
+++ algorithm/src/algorithm/Question2739.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Question2739 {
+	public static void main(String[] args) {
+		int N;
+		Scanner scanner = new Scanner(System.in);
+		N = scanner.nextInt();
+		for(int i=1;i<10;i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
+		
+		
+	}
+}
