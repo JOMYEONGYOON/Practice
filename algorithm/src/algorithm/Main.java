e2ba4956e7b7907ee987449a800671a595e1a0dc
hairh <hairh@DESKTOP-53J7LK2>
2020-08-11 18:43:42 +0900

    ㅇㅇ

--- algorithm/src/algorithm/Main.java
+++ algorithm/src/algorithm/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
 
+		int N;
+		Scanner scanner = new Scanner(System.in);
+		N = scanner.nextInt();
+		for(int i=1;i<10;i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
 		
 		
 	}
