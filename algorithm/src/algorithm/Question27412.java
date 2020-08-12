5df141220c3e9894ebd9faf1a56f076320b95ee1
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-08-12 21:13:27 +0900

    Create Question27412.java
    반복문 기초

---
+++ algorithm/src/algorithm/Question27412.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+    		
+    	int N;
+    	Scanner scanner = new Scanner(System.in);
+    	N = scanner.nextInt();
+    	for(int i=N;i>=1;i--) { // 5부터 거꾸러 출력 하는것
+    		// for(int i=1;i<=N;i++) 
+    		// 1 부터 시작하는거 출력 
+    		System.out.println(i);
+    		
+    	}
+    	
+    }
+}
