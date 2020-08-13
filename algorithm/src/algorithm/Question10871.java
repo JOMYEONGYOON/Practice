1beae85cd36107389d2577d93e66d55a696fad60
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-08-13 16:35:19 +0900

    8월13일커밋
    정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.

---
+++ algorithm/src/algorithm/Question10871.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+    		
+    	Scanner scanner = new Scanner(System.in);
+    	int N = scanner.nextInt();
+    	int X = scanner.nextInt();
+    	for(int i=1;i<=N;i++) {
+    	int A = scanner.nextInt();
+    	if(X>A) {
+    		
+    		System.out.print(A + " ");
+    		}
+    	}
+    	
+    }
+}
