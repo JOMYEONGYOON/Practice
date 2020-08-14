12ce73158b3b34b5d50c6b26b511d1a9d6b7c793
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-08-14 20:15:48 +0900

    8월14일커밋
    hasNextInt() 의 경우 입력값이 정수일경우 true를 반환하며 정수가 아닐경우 바로 예외를 던지며 더이상의 입력을 받지 않고 hasNextInt()에서 false를 반환하면서 반복문이 종료된다.

---
+++ algorithm/src/algorithm/Question10951.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Question10951 {
+    public static void main(String[] args)  {
+    	Scanner scanner = new Scanner(System.in);
+    	int A;
+    	int B;
+    	while(scanner.hasNextInt()) {
+    		 A = scanner.nextInt();
+        	 B = scanner.nextInt();
+    
+        		System.out.println(A+B);
+        	
+    	}
+    	
+    	
+    }
+
+}
