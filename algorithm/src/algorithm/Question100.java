55dc6bfcc631ad628f04d816b7ed425912b51309
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-08-21 17:43:36 +0900

    Create Question100.java

---
+++ algorithm/src/algorithm/Question100.java
@@ -0,0 +1,38 @@
+package algorithm;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double max = 0;
+		double avg = 0;
+		double sum = 0;
+		int n = scanner.nextInt();
+		double avgarr[] = new double[n];
+		for(int i =0; i<n; i++) {
+			double subject = scanner.nextInt();
+			
+			avgarr[i] = subject;
+			if(subject>max) {
+				
+				max= subject;
+	
+			}
+
+		}
+
+		  for(int j=0; j<n; j++) {
+			  avg = (avgarr[j]/max)*100;
+			  sum = avg+sum; 
+
+
+		  }
+		  
+		  double result = sum/n;	 
+		 System.out.println(result);
+
+	}
+}
