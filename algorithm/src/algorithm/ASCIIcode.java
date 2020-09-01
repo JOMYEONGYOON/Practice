4b2c699f99c431785d9e8cf61bde81a26b4add6e
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-09-01 20:53:39 +0900

    아스키코드

---
+++ algorithm/src/algorithm/ASCIIcode.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+
+import java.util.Scanner;
+
+public class ASCIIcode {
+ 
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     int ch = scanner.next().charAt(0);
+     System.out.println(ch);
+    	
+    }
+    
+}
