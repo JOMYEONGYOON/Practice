ad687d902bb1d9ed93343a451dd99a454cc2f5cb
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-09-12 16:51:17 +0900

    공부해야하는것
    ㅇㅇㅇ

--- algorithm/src/algorithm/Study1.java
+++ algorithm/src/algorithm/Study1.java
@@ -3,7 +3,7 @@ package algorithm;
 
 import java.util.Scanner;
 
-public class Study {
+public class Study1 {
  
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
